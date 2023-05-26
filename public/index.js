b91424d1fe45a91131592d46482c1a66df79a1df
s-egge <102624422+s-egge@users.noreply.github.com>
2023-05-26 16:26:23 -0700

    Added bucket fill functionality
    - Bucket button toggle added
    - Bucket fill functionality added for both left and right click (using respective colors)
    - Undid previous draw loop refactor

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@
 var gridSizeButton = document.getElementById("grid-size-button");
 var penButton = document.getElementById("pen");
 var eraserButton = document.getElementById("eraser");
+var bucketButton = document.getElementById("bucket");
 var deleteButton = document.getElementById("delete");
 var deleteYes = document.getElementById("delete-yes");
 var deleteNo = document.getElementById("delete-no");
@@ -13,7 +14,7 @@ var colorSwapperBtn = document.getElementById("swap-colors-btn");
 
 // canvas set-up
 var canvas = document.getElementById("canvas");
-var c = canvas.getContext('2d');
+var c = canvas.getContext('2d', { willReadFrequently: true });
 
 // pixel variables
 var gridSize = 0
@@ -41,18 +42,73 @@ function changeCurrentOnButton(newOnButton) {
         case newOnButton: //prevents infinite calls to this function
             return;
         case "pen":
-          penToggle();
-          break;
+            penToggle();
+            break;
         case "eraser":
-          eraserToggle();
-          break;
+            eraserToggle();
+            break;
+        case "bucket":
+            bucketToggle();
+            break;
       }
 
       currentOnButton = newOnButton;
 }
 
-function bucketFill() {
-    
+function getPixelColorFromCoordinates(x, y) {
+    const colorData = c.getImageData(x, y, 1, 1);
+    var r = colorData.data[0];
+    var g = colorData.data[1];
+    var b = colorData.data[2];
+    var a = colorData.data[3];
+
+    //special case where the color all white will return as black
+    if (a == 0)
+      return "#FFFFFF"
+
+    //otherwise convert rgb to hex
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+//checks if given coordinates are within the canvas bounds
+function withinCanvasBounds(x, y) {
+    if (x < 0 || x >= pixelLength * gridSize ||
+        y < 0 || y >= pixelLength * gridSize)
+        return false
+
+    else
+        return true
+}
+
+function bucketFill(colorToReplace, x, y) {
+    //fill pixel of passed in coordinates
+    c.beginPath();
+    c.fillStyle = pixelColor;
+    c.fillRect(x, y, pixelLength, pixelLength);
+    c.fill();
+
+    //check all surrounding pixels to see if they need to be replaced
+
+    //up
+    if(withinCanvasBounds(x, y - pixelLength) &&
+        getPixelColorFromCoordinates(x, y - pixelLength) == colorToReplace)
+        bucketFill(colorToReplace, x, y - pixelLength);
+
+    //down
+    if(withinCanvasBounds(x, y + pixelLength) &&
+        getPixelColorFromCoordinates(x, y + pixelLength) == colorToReplace)
+        bucketFill(colorToReplace, x, y + pixelLength);
+
+    //right
+    if(withinCanvasBounds(x + pixelLength, y) &&
+        getPixelColorFromCoordinates(x  + pixelLength, y) == colorToReplace)
+        bucketFill(colorToReplace, x + pixelLength, y);
+
+    //left
+    if(withinCanvasBounds(x - pixelLength, y) &&
+        getPixelColorFromCoordinates(x  - pixelLength, y) == colorToReplace)
+        bucketFill(colorToReplace, x - pixelLength, y);
+
 }
 
 /***********************************************************************************
@@ -67,14 +123,14 @@ function penToggle(){
     if(penOn) {
         document.getElementById("pen-img").src = "imgs/pencil-black.png"
         penOn = false
+        currentOnButton = "";
     }
 
     else{
         document.getElementById("pen-img").src = "imgs/pencil-white.png"
         penOn = true
+        changeCurrentOnButton("pen");
     }
-
-    changeCurrentOnButton("pen");
 }
 
 /***********************************************************************************
@@ -89,14 +145,39 @@ function eraserToggle(){
     if(eraserOn) {
         document.getElementById("eraser-img").src = "imgs/eraser-black.png"
         eraserOn = false
+        currentOnButton = "";
     }
 
     else{
         document.getElementById("eraser-img").src = "imgs/eraser-white.png"
         eraserOn = true
+        changeCurrentOnButton("eraser");
+    }
+}
+
+/***********************************************************************************
+ * If the bucket button is currently "on", remove "clicked" from classList, change
+ * icon to black, and set bool to false. If the bucket button is "off", add "clicked", 
+ * change icon to white, and set bool to true.
+ * *********************************************************************************/
+function bucketToggle(){
+    bucketButton.classList.toggle("clicked");
+
+    
+    //toggle eraser icon to black/white as needed
+    if(bucketOn) {
+        document.getElementById("bucket-img").src = "imgs/bucket-black.png"
+        bucketOn = false;
+        currentOnButton = "";
     }
 
-    changeCurrentOnButton("eraser");
+    else {
+        document.getElementById("bucket-img").src = "imgs/bucket-white.png"
+        bucketOn = true;
+        changeCurrentOnButton("bucket");
+    }
+
+    return;
 }
 
 /***********************************************************************************
@@ -123,10 +204,30 @@ function deleteToggle(){
     
 }
 
+
+/***********************************************************************************
+ * This reroutes when a user clicks on the canvas with "single click action" buttons, 
+ * meaning buttons where a user won't be dragging the mouse across the canvas
+ * *********************************************************************************/
+function canvasSingleClickActions() {
+    if (bucketOn) {
+        currentColor = getPixelColorFromCoordinates(pixelX, pixelY);
+
+        if (currentColor == pixelColor)
+            return
+
+        bucketFill(currentColor, pixelX, pixelY);
+    }
+}
+
 //add button functinality
 penButton.addEventListener('click', penToggle)
 eraserButton.addEventListener('click', eraserToggle)
+bucketButton.addEventListener('click', bucketToggle)
 deleteButton.addEventListener('click', deleteToggle)
+
+canvas.addEventListener('click', canvasSingleClickActions);
+canvas.addEventListener('contextmenu', canvasSingleClickActions);
 
 //change left and right colors on input
 leftColorPicker.oninput = function() {
@@ -175,7 +276,6 @@ deleteNo.addEventListener('click', deleteToggle)
 //when user clicks OK on grid size, update canvas pixel dimensions
 gridSizeButton.addEventListener('click', function(){
     var gridOptions = document.getElementsByName("grid-size");
-    var gridSize = 0
 
     //get selected grid size
     for(x = 0; x < gridOptions.length; x++) {
@@ -228,28 +328,22 @@ window.onmousemove = function (event) {
     pixelY = Math.floor((mouseY / pixelLength)) * pixelLength
 }
 
-//draw on current pixel with given color
-function draw(color) {
+//drawing loop for click and drag drawing/erasing
+function draw() {
+
+    if (mousePressed && (penOn || eraserOn)) {
         c.beginPath();
-        c.fillStyle = color;
+        c.fillStyle = pixelColor;
+
+        if(eraserOn)
+            c.fillStyle = "#FFFFFF"
+
         c.fillRect(pixelX, pixelY, pixelLength, pixelLength);
         c.fill();
-}
-
-//main loop, perform action based on clicking and currently pressed button
-function loop() {
-
-    if (mousePressed) {
-        if (penOn)
-            draw(pixelColor);
-        if (eraserOn)
-            draw("#FFFFFF")
-        if (bucketOn)
-            bucketFill()
     }
 
     //update canvas
-    window.requestAnimationFrame(loop);
+    window.requestAnimationFrame(draw);
 }
 
-loop();
+draw();
